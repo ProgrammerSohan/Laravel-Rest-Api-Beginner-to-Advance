294ad30f7ac23f3a88a71fb64fe4d09cecabfb72
ProgrammerSohan <sohanofficial2022@gmail.com>
2023-06-23 09:51:12 +0600

    epi-01-a-creating first api

---
+++ app/Http/Controllers/TestAPIController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestAPIController extends Controller
+{
+    public function firstAPI()
+    {
+        return "Response from controller";
+    }
+}
